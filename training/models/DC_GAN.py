7fc48de2d9ea1e8aac4b75ab21e29fab7a33427d
octal-zhihao <1187456954@qq.com>
2024-10-30 18:48:37 +0800

    fix: change img's size

--- training/models/DC_GAN.py
+++ training/models/DC_GAN.py
@@ -4,44 +4,62 @@ from torch import nn
 class DC_Generator(nn.Module):
     def __init__(self, latent_dim, image_channels=3):
         super(DC_Generator, self).__init__()
-        self.main = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),
+        self.fc = nn.Sequential(
+            nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),  # Output: [512, 4, 4]
             nn.BatchNorm2d(512),
             nn.ReLU(True),
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),         # Output: [256, 8, 8]
             nn.BatchNorm2d(256),
             nn.ReLU(True),
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),         # Output: [128, 16, 16]
             nn.BatchNorm2d(128),
             nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),          # Output: [64, 32, 32]
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, image_channels, 4, 2, 1, bias=False),
+            
+            nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),           # Output: [32, 64, 64]
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(32, image_channels, 4, 2, 1, bias=False), # Output: [image_channels, 128, 128]
             nn.Tanh()
         )
 
     def forward(self, x):
-        return self.main(x)
+        return self.fc(x)
+
+
 
 class DC_Discriminator(nn.Module):
     def __init__(self, image_channels=3):
         super(DC_Discriminator, self).__init__()
-        self.main = nn.Sequential(
-            nn.Conv2d(image_channels, 64, 4, 2, 1, bias=False),
+        self.fc = nn.Sequential(
+            nn.Conv2d(image_channels, 64, 4, 2, 1, bias=False),       # Output: [64, 64, 64]
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),                   # Output: [128, 32, 32]
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),                  # Output: [256, 16, 16]
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),                  # Output: [512, 8, 8]
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+            
+            nn.Conv2d(512, 1024, 4, 2, 1, bias=False),                 # Output: [1024, 4, 4]
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(1024, 1, 4, 1, 0, bias=False),                   # Output: [1, 1, 1]
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        return self.main(x).view(-1, 1).squeeze(1)
+        return self.fc(x).view(-1, 1).squeeze(1)
