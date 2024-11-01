2c3e9f2f2628219c024f82bebef4802f9cdb661a
EverlastingYuki <2063331724@qq.com>
2024-11-01 14:50:08 +0800

    feat: Support multiple pictures

--- Web/back_end/api/generate.py
+++ Web/back_end/api/generate.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from flask import request, Response
 from Web.back_end.api import api
 import json
@@ -15,6 +17,20 @@ def set_args(model_name, data):
         config = yaml.load(config_file, Loader=yaml.FullLoader)
         args.update(config['model'][model_name])
     return args
+
+
+def clear_dir(directory):
+    """清空文件夹"""
+    # 检查文件夹是否存在
+    if os.path.exists(directory) and os.path.isdir(directory):
+        # 清除文件夹内的所有文件和子文件夹
+        shutil.rmtree(directory)
+        # 重新创建文件夹
+        os.makedirs(directory, exist_ok=True)
+
+        print(f"已清空 {directory} 文件夹")
+    else:
+        print(f"{directory} 文件夹不存在")
 
 
 @api.route('generate', methods=['POST'])
@@ -37,7 +53,7 @@ def generate():
             "gen_search_num": 10,
             "gen_num": 5,
             "gen_mean": 0,
-            "gen_std": 1
+            "gen_`std": 1
         }
         ```
 
@@ -49,15 +65,24 @@ def generate():
         ```
         """
     data = request.form
+    save_dir = "Web/front_end/static"
+    clear_dir(save_dir)
     if 'DC_GAN' in data.get("model"):
         print('正在调用DC_GAN生成图片')
         args = set_args(model_name="DCGAN", data=data)
         model = MInterface.load_from_checkpoint(**args)
         model.eval()
         predict_demo(model, args['latent_dim'], save_dir="Web/front_end/static")
-
+    res = []
+    for root, dirs, files in os.walk(save_dir):
+        for file in files:
+            # 构建相对路径
+            relative_path = os.path.relpath(os.path.join(root, file), save_dir)
+            # 构建最终路径格式
+            final_path = f"./static/{relative_path}"
+            res.append(final_path)
     # 返回json类型字符串
     result = {
-        "res": ["./static/gan_img.png"]
+        "res": res
     }
     return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
