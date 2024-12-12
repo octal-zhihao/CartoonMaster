ced422d365fb8870e6555ff02280d98e520f1314
L <2210797528@qq.com>
2024-12-13 00:40:19 +0800

    简单优化DCGAN,实现UnrolledGAN

--- training/main.py
+++ training/main.py
@@ -5,19 +5,21 @@ from pytorch_lightning.loggers import CSVLogger, WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from data.data_interface import DInterface
 from models.model_interface import MInterface
+from models.Unrolled_model_interface import UnrolledMInterface
 import matplotlib.pyplot as plt
 from torchvision.utils import save_image
 import os
 import wandb
 
-def generate(model, latent_dim, save_dir="generated_images", generate_num=5):
+def predict_demo(model, latent_dim, save_dir="generated_images"):
     # 确保保存目录存在
     os.makedirs(save_dir, exist_ok=True)
     
     # 生成随机噪声
-    noise = torch.randn(generate_num, latent_dim)
+    noise = torch.randn(5, latent_dim)  # 生成 5 个随机噪声，形状为 [5, latent_dim]
     
-    noise = noise.view(generate_num, latent_dim, 1, 1)
+    # 将噪声张量的形状调整为 [5, latent_dim, 1, 1] 以适配生成器的输入
+    noise = noise.view(5, latent_dim, 1, 1)
     device = next(model.parameters()).device
     noise = noise.to(device)
     # 生成图像
@@ -37,14 +39,14 @@ if __name__ == "__main__":
     parser.add_argument("--data_path", type=str, default="./dataset/cartoon_color", help="Path to cartoon image dataset")
     parser.add_argument("--image_size", type=int, default=128, help="Size of the input image to the model")
     parser.add_argument("--latent_dim", type=int, default=100, help="Dimensionality of the latent space")
-    parser.add_argument("--batch_size", type=int, default=512, help="Training batch size")
-    parser.add_argument("--num_workers", type=int, default=8, help="Number of workers for data loaders")
+    parser.add_argument("--batch_size", type=int, default=32, help="Training batch size")
+    parser.add_argument("--num_workers", type=int, default=2, help="Number of workers for data loaders")
     parser.add_argument("--g_lr", type=float, default=0.0003, help="Generator learning rate")
     parser.add_argument("--d_lr", type=float, default=0.0001, help="Discriminator learning rate")
     parser.add_argument("--epochs", type=int, default=5000, help="Number of training epochs")
-    parser.add_argument("--model_name", type=str, default="DCGAN", help="Name of the model to train")
+    parser.add_argument("--model_name", type=str, default="Unrolled_DCGAN", help="Name of the model to train")
     parser.add_argument("--mode", type=str, default="train", help="Mode to run the script in: train or predict")
-    parser.add_argument("--checkpoint_path", type=str, default="my_checkpoint/epoch=199-step=6400.ckpt", help="Path to the model checkpoint for predictions")
+    parser.add_argument("--checkpoint_path", type=str, default="my_checkpoint/epoch=29-step=15300.ckpt", help="Path to the model checkpoint for predictions")
     args = parser.parse_args()
 
     
@@ -52,18 +54,23 @@ if __name__ == "__main__":
     if args.mode == "predict":
         # 初始化数据模块和模型
         data_module = DInterface(**vars(args))
-        model = MInterface.load_from_checkpoint(**vars(args))
+        if args.model_name == "Unrolled_DCGAN":
+            model = UnrolledMInterface.load_from_checkpoint(**vars(args))
+        else:
+            model = MInterface.load_from_checkpoint(**vars(args))
         model.eval()
         # 进行预测
-        generate(model, args.latent_dim, save_dir="generated_images")
+        predict_demo(model, args.latent_dim, save_dir="generated_images")
         exit()
     else:
         wandb.init(project="cartoon-gan", config=vars(args))
         wandb_logger = WandbLogger(project="cartoon-gan", config=vars(args))
         # 初始化数据模块和模型
         data_module = DInterface(**vars(args))
-        model = MInterface(**vars(args))
-
+        if args.model_name == "Unrolled_DCGAN":
+            model = UnrolledMInterface(**vars(args))
+        else:
+            model = MInterface(**vars(args))
         # 创建 ModelCheckpoint 回调
         checkpoint_callback = ModelCheckpoint(
             monitor='val_g_loss',                # 监控验证集损失
